910530532bb9e97c228fd7164fd21d4960bcc985
Maryam <maryamtamer510@gmail.com>
2026-03-11 16:58:08 +0200

    Add multiply function

--- calculator.py
+++ calculator.py
@@ -9,7 +9,9 @@ def subtract(a, b):
     """Return the difference of a and b."""
     return a - b
 
+def multiply(a, b):
 
+return a * b
 if _name_ == "_main_":
     print("Addition:", add(10, 5))
     print("Subtraction:", subtract(10, 5))
